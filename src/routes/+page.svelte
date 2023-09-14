91d54b799f8f7cb984151afc59770f7835de8ed4
이솜이 <122191856+somyiLee@users.noreply.github.com>
2023-09-14 16:53:22 +0900

    Feat: make todo maker

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,34 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import type { EventHandler, ChangeEventHandler } from "svelte/elements";
+
+let todoList: string[] = [];
+let todo = "";
+
+const submitHandler: EventHandler <SubmitEvent, HTMLFormElement> = () => {
+  todoList=[...todoList, todo];
+  todo = "";
+}
+
+const inputHandler: ChangeEventHandler<HTMLInputElement>  = (e) => {
+  const {value} = e.target as HTMLInputElement;
+  todo = value;
+};
+
+
+</script>
+
+<main>
+<h1>Make Todo List</h1>
+
+<form on:submit|preventDefault={submitHandler}>
+  <input type="text" placeholder="write to do item" on:change={inputHandler} bind:value={todo}/>
+  <button type="submit">click</button>
+</form>
+
+<ul>
+  {#each todoList as todoList}
+  <li>{todoList}</li>
+  {/each}
+</ul>
+</main>
+
